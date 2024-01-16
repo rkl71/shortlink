4088dbd69fabd27830e44dc396ec15221884dfc8
rkl71 <renkelin00@gmail.com>
2024-01-16 14:53:11 +0800

    用户信息脱敏实现

--- admin/src/main/java/com/hanyang/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.hanyang.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.hanyang.shortlink.admin.common.convention.result.Result;
 import com.hanyang.shortlink.admin.common.convention.result.Results;
-import com.hanyang.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.hanyang.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.hanyang.shortlink.admin.dto.resp.UserRespDTO;
 import com.hanyang.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -24,12 +25,15 @@ public class UserController {
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
-        UserRespDTO result = userService.getUserByUsername(username);
-        if (result == null) {
-            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
-        } else {
-            return Results.success(result);
-        }
+        return Results.success(userService.getUserByUsername(username));
     }
 
+    /**
+     * 根据用户名查询无脱敏用户信息
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
+    }
 }
+
--- admin/src/main/java/com/hanyang/shortlink/admin/dto/resp/UserActualRespDTO.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/dto/resp/UserActualRespDTO.java
@@ -1,14 +1,12 @@
 package com.hanyang.shortlink.admin.dto.resp;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.hanyang.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
  * 用户返回参数响应
  */
 @Data
-public class UserRespDTO {
+public class UserActualRespDTO {
     /**
      * id
      */
@@ -28,7 +26,6 @@ public class UserRespDTO {
     /**
      * 手机号
      */
-    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
