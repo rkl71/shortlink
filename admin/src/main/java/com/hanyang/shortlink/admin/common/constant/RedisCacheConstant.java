3d6c784c90ad05540b2cac399a97ea4ae4be6c61
rkl71 <renkelin00@gmail.com>
2024-01-16 21:32:17 +0800

    通过分布式锁完善用户注册功能

---
+++ admin/src/main/java/com/hanyang/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,9 @@
+package com.hanyang.shortlink.admin.common.constant;
+
+/**
+ * Redis 缓存常量类
+ */
+public class RedisCacheConstant {
+
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/com/hanyang/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/service/impl/UserServiceImpl.java
@@ -13,9 +13,12 @@ import com.hanyang.shortlink.admin.dto.resp.UserRespDTO;
 import com.hanyang.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import static com.hanyang.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 import static com.hanyang.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
 import static com.hanyang.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
@@ -27,6 +30,8 @@ import static com.hanyang.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SA
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
+
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
@@ -50,10 +55,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUsername(requestParam.getUsername())){
             throw new ClientException(USER_NAME_EXIST);
         }
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if(inserted < 1){
-            throw new ClientException(USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()){
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if(inserted < 1){
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        }  finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+
+
     }
 }
