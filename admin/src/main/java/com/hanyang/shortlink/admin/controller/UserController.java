d0b69601827c0a2190d649e564b9d74bfab1aaa5
rkl71 <renkelin00@gmail.com>
2024-01-17 22:25:48 +0800

    用户系统登录功能实现

--- admin/src/main/java/com/hanyang/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/controller/UserController.java
@@ -3,9 +3,11 @@ package com.hanyang.shortlink.admin.controller;
 import cn.hutool.core.bean.BeanUtil;
 import com.hanyang.shortlink.admin.common.convention.result.Result;
 import com.hanyang.shortlink.admin.common.convention.result.Results;
+import com.hanyang.shortlink.admin.dto.req.UserLoginReqDTO;
 import com.hanyang.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.hanyang.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.hanyang.shortlink.admin.dto.resp.UserActualRespDTO;
+import com.hanyang.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.hanyang.shortlink.admin.dto.resp.UserRespDTO;
 import com.hanyang.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -60,6 +62,22 @@ public class UserController {
     public Result<Void> update(@RequestBody UserUpdateReqDTO requestParam){
         userService.update(requestParam);
         return Results.success();
+    }
+
+    /**
+     * 用户登录
+     */
+    @PostMapping("/api/short-link/v1/user/login")
+    public Result<UserLoginRespDTO> login(@RequestBody UserLoginReqDTO requestParam){
+        return Results.success(userService.login(requestParam));
+    }
+
+    /**
+     * 检查用户是否登录
+     */
+    @GetMapping("/api/short-link/v1/user/check-login")
+    public Result<Boolean> checkLogin(@RequestParam("username") String username, @RequestParam("token") String token){
+        return Results.success(userService.checkLogin(username, token));
     }
 }
 
--- admin/src/main/java/com/hanyang/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hanyang.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -8,8 +9,10 @@ import com.hanyang.shortlink.admin.common.convention.exception.ClientException;
 import com.hanyang.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.hanyang.shortlink.admin.dao.entity.UserDO;
 import com.hanyang.shortlink.admin.dao.mapper.UserMapper;
+import com.hanyang.shortlink.admin.dto.req.UserLoginReqDTO;
 import com.hanyang.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.hanyang.shortlink.admin.dto.req.UserUpdateReqDTO;
+import com.hanyang.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.hanyang.shortlink.admin.dto.resp.UserRespDTO;
 import com.hanyang.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +20,11 @@ import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import static com.hanyang.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 import static com.hanyang.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
@@ -32,6 +39,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
     private final RedissonClient redissonClient;
+    private final StringRedisTemplate stringRedisTemplate;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -53,21 +61,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public void register(UserRegisterReqDTO requestParam) {
-        if (!hasUsername(requestParam.getUsername())){
+        if (!hasUsername(requestParam.getUsername())) {
             throw new ClientException(USER_NAME_EXIST);
         }
         RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
         try {
-            if (lock.tryLock()){
+            if (lock.tryLock()) {
                 int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-                if(inserted < 1){
+                if (inserted < 1) {
                     throw new ClientException(USER_SAVE_ERROR);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
                 return;
             }
             throw new ClientException(USER_NAME_EXIST);
-        }  finally {
+        } finally {
             lock.unlock();
         }
     }
@@ -78,5 +86,30 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         LambdaQueryWrapper<UserDO> updateWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, requestParam.getUsername());
         baseMapper.update(BeanUtil.toBean(requestParam, UserDO.class), updateWrapper);
+    }
+
+    @Override
+    public UserLoginRespDTO login(UserLoginReqDTO requestParam) {
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
+                .eq(UserDO::getUsername, requestParam.getUsername())
+                .eq(UserDO::getPassword, requestParam.getPassword())
+                .eq(UserDO::getDelFlag, 0);
+        UserDO userDO = baseMapper.selectOne(queryWrapper);
+        if (userDO == null) {
+            throw new ClientException("用户不存在");
+        }
+        Boolean hasLogin = stringRedisTemplate.hasKey("login_" + requestParam.getUsername());
+        if (hasLogin != null && hasLogin) {
+            throw new ClientException("用户已登录");
+        }
+        String uuid = UUID.randomUUID().toString();
+        stringRedisTemplate.opsForHash().put("login_" + requestParam.getUsername(), uuid, JSON.toJSONString(userDO));
+        stringRedisTemplate.expire("login_" + requestParam.getUsername(), 30L, TimeUnit.MINUTES);
+        return new UserLoginRespDTO(uuid);
+    }
+
+    @Override
+    public Boolean checkLogin(String username, String token) {
+        return stringRedisTemplate.opsForHash().get("login_" + username, token) != null;
     }
 }
