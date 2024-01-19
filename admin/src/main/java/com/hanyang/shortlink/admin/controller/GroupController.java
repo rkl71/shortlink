cea43443cbd93fd78c0986be39e341eca6e953ed
rkl71 <renkelin00@gmail.com>
2024-01-19 12:09:46 +0800

    短链接系统分组集合实现

--- admin/src/main/java/com/hanyang/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/controller/GroupController.java
@@ -3,11 +3,15 @@ package com.hanyang.shortlink.admin.controller;
 import com.hanyang.shortlink.admin.common.convention.result.Result;
 import com.hanyang.shortlink.admin.common.convention.result.Results;
 import com.hanyang.shortlink.admin.dto.req.ShortLinkGroupReqDTO;
+import com.hanyang.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.hanyang.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 短链接分组控制层
@@ -18,9 +22,17 @@ public class GroupController {
 
     private  final GroupService  groupService;
 
+    /**
+     * 新增短链接分组
+     */
     @PostMapping("/api/short-link/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupReqDTO requestParam){
         groupService.saveGroup(requestParam.getName());
         return Results.success();
+    }
+
+    @GetMapping("/api/short-link/v1/group")
+    public Result<List<ShortLinkGroupRespDTO>> listGroup(){
+        return Results.success(groupService.listGroup());
     }
 }
--- admin/src/main/java/com/hanyang/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.hanyang.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hanyang.shortlink.admin.dao.entity.GroupDO;
 import com.hanyang.shortlink.admin.dao.mapper.GroupMapper;
+import com.hanyang.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.hanyang.shortlink.admin.service.GroupService;
 import com.hanyang.shortlink.admin.toolkit.RandomGenerator;
 import groovy.util.logging.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 短链接分组接口实现层
@@ -28,6 +32,17 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
                 .sortOrder(0)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                // TODO 从当前上下文获取用户名
+                .eq(GroupDO::getUsername, "renkelin")
+                .orderByAsc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
