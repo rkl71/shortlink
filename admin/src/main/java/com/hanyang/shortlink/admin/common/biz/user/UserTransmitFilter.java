a30c6de8def1775cb1137988e9f692ef80ffc27c
rkl71 <renkelin00@gmail.com>
2024-01-19 15:15:50 +0800

    用户登录拦截器中忽略登录接口

--- admin/src/main/java/com/hanyang/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -4,9 +4,12 @@ import com.alibaba.fastjson2.JSON;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.apache.curator.shaded.com.google.common.collect.Lists;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 
 /**
@@ -17,15 +20,23 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/admin/v1/user/login",
+            "/api/short-link/admin/v1/user/has-username"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-        if (userInfoJsonStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!Objects.equals(requestURI, "/api/short-link/v1/user/login")) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -33,4 +44,13 @@ public class UserTransmitFilter implements Filter {
             UserContext.removeUser();
         }
     }
+
+//    private void returnJson(HttpServletResponse response, String json) throws Exception {
+//        response.setCharacterEncoding("UTF-8");
+//        response.setContentType("text/html; charset=utf-8");
+//        try (PrintWriter writer = response.getWriter()) {
+//            writer.print(json);
+//        }
+//    }
 }
+        
