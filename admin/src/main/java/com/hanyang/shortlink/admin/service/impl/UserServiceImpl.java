d3b9c9cb3bf53ea7e113973d5647eb19bee8ae9d
rkl71 <renkelin00@gmail.com>
2024-01-23 23:15:43 +0800

    注册用户后默认创建对应分组

--- admin/src/main/java/com/hanyang/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ import com.hanyang.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.hanyang.shortlink.admin.dto.req.UserUpdateReqDTO;
 import com.hanyang.shortlink.admin.dto.resp.UserLoginRespDTO;
 import com.hanyang.shortlink.admin.dto.resp.UserRespDTO;
+import com.hanyang.shortlink.admin.service.GroupService;
 import com.hanyang.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
@@ -40,6 +41,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
     private final RedissonClient redissonClient;
     private final StringRedisTemplate stringRedisTemplate;
+    private final GroupService  groupService;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -76,12 +78,14 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
                     throw new ClientException(USER_EXIST);
                 }
                 userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                groupService.saveGroup("默认分组");
                 return;
             }
             throw new ClientException(USER_NAME_EXIST);
         } finally {
             lock.unlock();
         }
+
     }
 
     @Override
