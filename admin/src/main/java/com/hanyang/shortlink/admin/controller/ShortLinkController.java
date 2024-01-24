daa73c77d040c71496cabc52e598a1ca8de5be5b
rkl71 <renkelin00@gmail.com>
2024-01-24 23:41:49 +0800

    短链接后管短链接修改功能实现

--- admin/src/main/java/com/hanyang/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/controller/ShortLinkController.java
@@ -2,15 +2,14 @@ package com.hanyang.shortlink.admin.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.hanyang.shortlink.admin.common.convention.result.Result;
+import com.hanyang.shortlink.admin.common.convention.result.Results;
 import com.hanyang.shortlink.admin.remote.ShortLinkRemoteService;
 import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 短链接后管控制层
@@ -30,6 +29,15 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/admin/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PutMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkRemoteService.updateShortLink(requestParam);
+        return Results.success();
     }
 
 
--- admin/src/main/java/com/hanyang/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.hanyang.shortlink.admin.common.convention.result.Result;
 import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -30,6 +31,16 @@ public interface ShortLinkRemoteService {
         String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create", JSON.toJSONString(requestParam));
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
+    }
+
+    /**
+     * 修改短链接
+     *
+     * @param requestParam 修改短链接请求参数
+     * @return 短链接修改响应
+     */
+    default void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
+        HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/update", JSON.toJSONString(requestParam));
     }
 
     /**
@@ -61,4 +72,6 @@ public interface ShortLinkRemoteService {
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
     }
+
+
 }
