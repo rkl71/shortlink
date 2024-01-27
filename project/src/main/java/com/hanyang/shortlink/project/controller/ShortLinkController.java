a9e1809bd3391a1c63e378eeb199f90608615298
rkl71 <renkelin00@gmail.com>
2024-01-27 22:31:37 +0800

    短链接跳转原始链接添加方法注释

--- project/src/main/java/com/hanyang/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/hanyang/shortlink/project/controller/ShortLinkController.java
@@ -26,6 +26,9 @@ public class ShortLinkController {
 
     private final ShortLinkService shortLinkService;
 
+    /**
+     * 短链接跳转原始链接
+     */
     @GetMapping("/{short-uri}")
     public void restoreUrl(@PathVariable("short-uri") String shortUri, ServletRequest request, ServletResponse response){
         shortLinkService.restoreUrl(shortUri, request, response);
