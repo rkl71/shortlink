89bcf491589c75391c2a00dcf58ac0a62228df65
rkl71 <renkelin00@gmail.com>
2024-01-28 17:25:01 +0800

    获取目标网站标题功能实现

--- admin/src/main/java/com/hanyang/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -11,6 +11,7 @@ import com.hanyang.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
@@ -70,6 +71,18 @@ public interface ShortLinkRemoteService {
         requestMap.put("requestParam", requestParam);
         String resultPageStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 根据 URL 获取标题
+     *
+     * @param url 目标网站地址
+     * @return 网站标题
+     */
+    default Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        String resultStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/title?url=" + url);
+        return JSON.parseObject(resultStr, new TypeReference<>() {
         });
     }
 
