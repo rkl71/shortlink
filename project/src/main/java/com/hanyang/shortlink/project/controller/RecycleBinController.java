0c747e1393a06244d89523ade8450a3af882b497
rkl71 <renkelin00@gmail.com>
2024-01-29 10:54:40 +0800

    回收站分页查询功能实现

--- project/src/main/java/com/hanyang/shortlink/project/controller/RecycleBinController.java
+++ project/src/main/java/com/hanyang/shortlink/project/controller/RecycleBinController.java
@@ -1,10 +1,14 @@
 package com.hanyang.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.hanyang.shortlink.project.common.convention.result.Result;
 import com.hanyang.shortlink.project.common.convention.result.Results;
 import com.hanyang.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+import com.hanyang.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.hanyang.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import com.hanyang.shortlink.project.service.RecycleBinService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +22,20 @@ public class RecycleBinController {
 
     private final RecycleBinService recycleBinService;
 
+    /**
+     * 保存回收站
+     */
     @PostMapping("/api/short-link/v1/recycle-bin/save")
     public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
         recycleBinService.saveRecycleBin(requestParam);
         return Results.success();
+    }
+
+    /**
+     * 分页查询回收站短链接
+     */
+    @GetMapping("/api/short-link/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return Results.success(recycleBinService.pageShortLink(requestParam));
     }
 }
--- project/src/main/java/com/hanyang/shortlink/project/service/RecycleBinService.java
+++ project/src/main/java/com/hanyang/shortlink/project/service/RecycleBinService.java
@@ -1,8 +1,11 @@
 package com.hanyang.shortlink.project.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.hanyang.shortlink.project.dao.entity.ShortLinkDO;
 import com.hanyang.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+import com.hanyang.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import com.hanyang.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 
 /**
  * 回收站管理接口层
@@ -15,4 +18,12 @@ public interface RecycleBinService extends IService<ShortLinkDO> {
      * @param requestParam 请求参数
      */
     void saveRecycleBin(RecycleBinSaveReqDTO requestParam);
+
+    /**
+     * 分页查询短链接
+     *
+     * @param requestParam 分页查询短链接请求参数
+     * @return 短链接分页返回结果
+     */
+    IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkPageReqDTO requestParam);
 }
