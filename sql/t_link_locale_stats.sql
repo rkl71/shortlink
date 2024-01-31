f4c19cb5182547f7fd044f0e0c6ecff901732ec9
rkl71 <renkelin00@gmail.com>
2024-01-31 12:31:59 +0800

    短链接地区表格创建SQL语句

---
+++ sql/t_link_locale_stats.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `t_link_locale_stats` (
+                                       `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                                       `full_short_url` varchar(128) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '完整短链接',
+                                       `gid` varchar(32) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分组标识',
+                                       `date` date DEFAULT NULL COMMENT '日期',
+                                       `cnt` int DEFAULT NULL COMMENT '访问量',
+                                       `province` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '省份名称',
+                                       `city` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '市名称',
+                                       `adcode` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '城市编码',
+                                       `country` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '国家标识',
+                                       `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                       `update_time` datetime NOT NULL COMMENT '修改时间',
+                                       `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0表示删除 1表示未删除',
+                                       PRIMARY KEY (`id`),
+                                       UNIQUE KEY `idx_unique_locale_stats` (`full_short_url`,`gid`,`date`,`adcode`,`province`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
