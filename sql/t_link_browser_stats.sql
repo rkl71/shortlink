ed61ca83238f9bb6f02d16e03e7a936d22e09280
rkl71 <renkelin00@gmail.com>
2024-01-31 13:51:50 +0800

    短链接监控浏览器统计表格SQL语句创建

---
+++ sql/t_link_browser_stats.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `t_link_browser_stats` (
+                                        `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                                        `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+                                        `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识',
+                                        `date` date DEFAULT NULL COMMENT '日期',
+                                        `cnt` int(11) DEFAULT NULL COMMENT '访问量',
+                                        `browser` varchar(64) DEFAULT NULL COMMENT '浏览器',
+                                        `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                        `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+                                        `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',
+                                        PRIMARY KEY (`id`),
+                                        UNIQUE KEY `idx_unique_browser_stats` (`full_short_url`,`gid`,`date`,`browser`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
