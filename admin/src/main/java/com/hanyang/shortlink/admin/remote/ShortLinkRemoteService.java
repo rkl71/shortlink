e94a23b7a06da7abe8b84c53176ba35ca88cfb56
rkl71 <renkelin00@gmail.com>
2024-02-02 15:10:40 +0800

    短链接监控访问记录分页功能实现

--- admin/src/main/java/com/hanyang/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -10,10 +10,7 @@ import com.hanyang.shortlink.admin.dto.req.RecycleBinRemoveReqDTO;
 import com.hanyang.shortlink.admin.dto.req.RecycleBinRestoreReqDTO;
 import com.hanyang.shortlink.admin.dto.req.RecycleBinSaveReqDTO;
 import com.hanyang.shortlink.admin.remote.dto.req.*;
-import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
-import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
-import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import com.hanyang.shortlink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
+import com.hanyang.shortlink.admin.remote.dto.resp.*;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
@@ -139,6 +136,21 @@ public interface ShortLinkRemoteService {
      */
     default Result<ShortLinkStatsRespDTO> oneShortLinkStats(ShortLinkStatsReqDTO requestParam) {
         String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats", BeanUtil.beanToMap(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 访问单个短链接指定时间内监控访问记录数据
+     *
+     * @param requestParam 访问短链接监控访问记录请求参数
+     * @return 短链接监控访问记录信息
+     */
+    default Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        Map<String, Object> stringObjectMap = BeanUtil.beanToMap(requestParam, false, true);
+        stringObjectMap.remove("orders");
+        stringObjectMap.remove("records");
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats/access-record", stringObjectMap);
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
     }
--- project/src/main/java/com/hanyang/shortlink/project/controller/ShortLinkStatsController.java
+++ project/src/main/java/com/hanyang/shortlink/project/controller/ShortLinkStatsController.java
@@ -1,8 +1,11 @@
 package com.hanyang.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.hanyang.shortlink.project.common.convention.result.Result;
 import com.hanyang.shortlink.project.common.convention.result.Results;
+import com.hanyang.shortlink.project.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.hanyang.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.hanyang.shortlink.project.dto.resp.ShortLinkStatsAccessRecordRespDTO;
 import com.hanyang.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
 import com.hanyang.shortlink.project.service.ShortLinkStatsService;
 import lombok.RequiredArgsConstructor;
@@ -24,5 +27,13 @@ public class ShortLinkStatsController {
     @GetMapping("/api/short-link/v1/stats")
     public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
         return Results.success(shortLinkStatsService.oneShortLinkStats(requestParam));
+    }
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/v1/stats/access-record")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        return Results.success(shortLinkStatsService.shortLinkStatsAccessRecord(requestParam));
     }
 }
