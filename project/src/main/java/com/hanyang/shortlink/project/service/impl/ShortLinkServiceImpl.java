e013332a9f192aa2ae5dbee19a2f6b14ed363f78
rkl71 <renkelin00@gmail.com>
2024-02-03 22:17:11 +0800

    短链接新增时初始化访问统计为0

--- project/src/main/java/com/hanyang/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/hanyang/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -100,6 +100,9 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .describe(requestParam.getDescribe())
                 .shortUri(shortLinkSuffix)
                 .enableStatus(0)
+                .totalPv(0)
+                .totalUv(0)
+                .totalUip(0)
                 .fullShortUrl(fullShortUrl)
                 .favicon(getFavicon(requestParam.getOriginUrl()))
                 .build();
