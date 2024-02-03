22cc9216411bccf3283ca055f2fd27e4ca322a62
rkl71 <renkelin00@gmail.com>
2024-02-03 22:49:27 +0800

    短链接监控今日统计实现

--- project/src/main/java/com/hanyang/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/hanyang/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,12 +2,18 @@ package com.hanyang.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.hanyang.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -43,5 +49,5 @@ public class LinkStatsTodayDO extends BaseDO {
     /**
      * 今日ip数
      */
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
