8ed57d563e241d32aacdbf537a77e197fed80f82
rkl71 <renkelin00@gmail.com>
2024-02-13 09:58:22 +0800

    发布短链接演示环境优化

--- admin/src/main/java/com/hanyang/shortlink/admin/common/web/GlobalExceptionHandler.java
+++ admin/src/main/java/com/hanyang/shortlink/admin/common/web/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.hanyang.shortlink.admin.common.web;
 
 import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.hanyang.shortlink.admin.common.convention.errorcode.BaseErrorCode;
 import com.hanyang.shortlink.admin.common.convention.exception.AbstractException;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -63,6 +65,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Throwable.class)
     public Result defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
         log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        // 注意，此处是为了聚合模式添加的代码，正常不需要该判断
+        if (Objects.equals(throwable.getClass().getSuperclass().getSimpleName(), AbstractException.class.getSimpleName())) {
+            String errorCode = ReflectUtil.getFieldValue(throwable, "errorCode").toString();
+            String errorMessage = ReflectUtil.getFieldValue(throwable, "errorMessage").toString();
+            return Results.failure(errorCode, errorMessage);
+        }
         return Results.failure();
     }
 
