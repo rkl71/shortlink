33c50b9efa8e41d96dd503ffbf75332077fc264a
rkl71 <renkelin00@gmail.com>
2024-02-14 17:27:04 +0800

    修复短链接分页问题

--- project/src/main/java/com/hanyang/shortlink/project/config/DataBaseConfiguration.java
+++ project/src/main/java/com/hanyang/shortlink/project/config/DataBaseConfiguration.java
@@ -3,7 +3,7 @@ package com.hanyang.shortlink.project.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +17,7 @@ public class DataBaseConfiguration {
      * 分页插件
      */
     @Bean
-    @ConditionalOnBean
+    @ConditionalOnMissingBean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
